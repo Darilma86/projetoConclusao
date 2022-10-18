d3c74abd40ec9b5c5d0ab1001130fc90cab7f507
Darilma86 <darydejesus86@gmail.com>
2022-10-18 18:07:04 -0300

    tela relatorio de compra

--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,8 @@ Rails.application.routes.draw do
 
   resources :relatorios
 
+  resources :relcompras
+
   root "home#index"  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
