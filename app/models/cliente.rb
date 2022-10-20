f8912439351c09967ffcdd4b754b6155eb5c1375
Darilma86 <darydejesus86@gmail.com>
2022-10-19 22:45:20 -0300

    "validacao cliente e produto"

--- app/models/cliente.rb
+++ app/models/cliente.rb
@@ -5,4 +5,6 @@ class Cliente < ApplicationRecord
 
     accepts_nested_attributes_for :vendas, :produtos, allow_destroy: true
 
+    validates :name, :rg, :cpf, :celucar, :email, :cidade, presence: true
+    
 end
--- app/models/produto.rb
+++ app/models/produto.rb
@@ -3,4 +3,6 @@ class Produto < ApplicationRecord
     has_many :vendas, inverse_of: :produto, dependent: :destroy
     has_many :clientes, through: :vendas
 
+    validates :nome, :quantidade, :tipo, :numero, :preco_venda, :preco_compra, :descricao, presence: true
+
 end
