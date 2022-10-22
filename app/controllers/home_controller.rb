2a96b330f8ead5371a020410bd5cf1b739ffcc11
Darilma86 <darydejesus86@gmail.com>
2022-10-22 16:30:25 -0300

    "total de cliente e produtos

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,9 +3,11 @@ class HomeController < ApplicationController
 
   def index
     @total_vendas = Compra.all.count
-
+    @total_clientes = Cliente.all.count
+    @total_produtos = Produto.all.count
+   
 
     @vendas = Venda.all.order(created_at: :desc).limit(5)
-    
+  
   end
 end
