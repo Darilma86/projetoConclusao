7e8a8728badb9cc6532e323fb6906ee98ca9c2a8
Darilma86 <darydejesus86@gmail.com>
2022-10-23 17:27:08 -0300

    "botao imprimir"

--- app/models/cliente.rb
+++ app/models/cliente.rb
@@ -5,6 +5,11 @@ class Cliente < ApplicationRecord
 
     accepts_nested_attributes_for :vendas, :produtos, allow_destroy: true
 
-    validates :name, :rg, :cpf, :celucar, :email, :cidade, presence: true
-    
+    validates :name, :celucar, :email, :cidade, presence: { message: 'deve ser preenchido'}
+    validates :rg, presence: { message: 'deve ser preenchido'},
+    length: {minimum: 10, maximum: 10, message: 'Deve conter 10 dígitos'}
+
+    validates :cpf, presence: {message: 'deve ser preenchido'},
+    length: {minimum: 14, maximum: 14}, uniqueness: {message: 'Esse cpf já está cadastrado'}
+
 end
